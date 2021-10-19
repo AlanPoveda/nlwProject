337c5c267aae899b56749595ecb913d71d5ca7b9
Alan Poveda <alsepoveda@gmail.com>
2021-10-18 21:27:49 -0300

    Crete routes for authentication and call back, redirect route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
-import express from "express";
+import "dotenv/config";
+import express, { response } from "express";
 
 const app = express();
+
+//Route Github para fazer a sua autenticação
+app.get("/github", (req,res)=>{
+    res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+});
+
+//Rota que redireciona depois da autenticação
+app.get("/signin/callback", (req,res)=>{
+    //Pegar o codigo de autenticação que é gerado pelo próprio git
+    const { code } =  req.query;
+
+    return res.json(code);
+});
+
 
 app.listen(4000, ()=>{
     console.log("🎉 Server is run in port 4000");
 })
+
