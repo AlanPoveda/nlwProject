d8889164215c124b8ca26629233bef919b34c9dc
Alan Poveda <alsepoveda@gmail.com>
2021-10-19 08:06:33 -0300

    Create user token, error message if not authenticate, save in db

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import prismaClient from "../prisma"
+import { sign } from "jsonwebtoken"
 
 /**
  * Receber code(string)
@@ -66,7 +67,22 @@ class AuthenticateUserService{
             })
         }
 
-        return response.data;
+        const token = sign(
+            {
+            user:{
+                name: user.name,
+                avatar_url: user.avatar_url,
+                id: user.id
+            }
+            },
+            process.env.JWT_SECRET,
+            {
+                subject:user.id,
+                expiresIn: "1d"
+            }
+        )
+
+        return { token, user };
 
     }
 
